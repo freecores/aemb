80bf0ce6574b438065dfdae6eec9ab97a54e53ae
sybreon <sybreon@55ffc1f0-3e4a-42ed-888c-7774f35025f9>
2007-04-04 06:07:45 +0000

    Fixed C code bug which passes the test

--- sw/c/aeMB_testbench.c
+++ sw/c/aeMB_testbench.c
@@ -1,6 +1,7 @@
 /*
- * $Id: aeMB_testbench.c,v 1.1 2007-03-09 17:41:57 sybreon Exp $
+ * $Id: aeMB_testbench.c,v 1.2 2007-04-04 06:07:45 sybreon Exp $
  * 
+ * AEMB Function Verification C Testbench
  * Copyright (C) 2006 Shawn Tan Ser Ngiap <shawn.tan@aeste.net>
  *  
  * This library is free software; you can redistribute it and/or modify it 
@@ -18,44 +19,109 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  *
  * DESCRIPTION
- * Controls the state of the processor.
+ * Runs a simple test programme that calculates fibonnaci numbers using two
+ * different methods. It tests a whole gamut of operations and is tightly
+ * linked to the ae68_testbench.v testbench module for verification.
+ *
+ * The fibonnaci code is from 
+ * http://en.literateprograms.org/Fibonacci_numbers_(C)
  * 
  * HISTORY
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2007/03/09 17:41:57  sybreon
+ * initial import
+ *
  */
 
-#include <stdlib.h>
+/* Special Prototypes */
+void int_call_func () __attribute__((save_volatiles));
+void int_handler_func () __attribute__ ((interrupt_handler));
 
-int main () {
-
-  int a;
-  int b;
-  int c[10];
-  register int i;
-
-  a = rand();
-  b = rand();
-
-  c[0] = add_test(a,b);
-  c[1] = sub_test(a,b);
-  c[2] = mul_test(a,b);
-  c[3] = div_test(a,b);
-
-  for (i=0;i<5;i++) {
-	c[i+5] = c[i];
-  }
-
-  return c[3];
+/* Interrupt Handler */
+void int_handler_func () {
+  int_call_func();
 }
 
-// RES = B - A
-int sub_test(int a, int b) { return (b-a); }
+void int_call_func () {
+  while (1) {}
+}
 
-// RES = A + B
-int add_test(int a, int b) { return (a+b); }
+/* Recursive Version */
 
-// RES = A * B
-int mul_test(int a, int b) { return (a*b); }
+unsigned int slowfib(unsigned int n)
+{
+  return n < 2 ? n : slowfib(n-1) + slowfib(n-2);
+}
 
-// RES = B / A
-int div_test(int a, int b) { return (b/a); }
+/* Iterative Version */
+
+unsigned int fastfib(unsigned int n)
+{
+  unsigned int a[3];
+  unsigned int *p=a;
+  unsigned int i;
+  
+  for(i=0; i<=n; ++i) {
+    if(i<2) *p=i;
+    else {
+      if(p==a) *p=*(a+1)+*(a+2);
+      else if(p==a+1) *p=*a+*(a+2);
+      else *p=*a+*(a+1);
+    }
+    if(++p>a+2) p=a;
+  }
+  
+  return p==a?*(p+2):*(p-1);
+}
+
+/* Compare the Results */
+
+int main() {
+  unsigned int n;
+  unsigned int fib_fast, fib_slow;  
+  unsigned int fib_lut[] = {0,
+			    1,
+			    1,
+			    2,
+			    3,
+			    5,
+			    8,
+			    13,
+			    21,
+			    34,
+			    55,
+			    89,
+			    144,
+			    233,
+			    377,
+			    610,
+			    987,
+			    1597,
+			    2584,
+			    4181,
+			    6765,
+			    10946,
+			    17711,
+			    28657,
+			    46368,
+			    75025,
+			    121393,
+			    196418,
+			    317811,
+			    514229,
+			    832040,
+			    1346269,
+			    2178309,
+			    3524578,
+			    5702887};  
+  
+  for (n=0;n<35;n++) {
+    fib_slow = slowfib(n);    
+    fib_fast = fastfib(n);
+    while ((fib_fast != fib_lut[n]) || (fib_slow != fib_fast)) {
+      fib_lut[n] = 0x00ED17FA;
+    }
+  }    
+  
+  return fib_fast;
+}
