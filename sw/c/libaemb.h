fe2a7fbac0d8df2246ccb167609ef51e401611b9
sybreon <sybreon@55ffc1f0-3e4a-42ed-888c-7774f35025f9>
2007-12-16 03:26:37 +0000

    Made T0 loop.

--- sw/c/libaemb.h
+++ sw/c/libaemb.h
@@ -1,4 +1,4 @@
-/* $Id: libaemb.h,v 1.2 2007-12-12 19:17:00 sybreon Exp $
+/* $Id: libaemb.h,v 1.3 2007-12-16 03:26:37 sybreon Exp $
 **
 ** AEMB2 CUSTOM LIBRARY
 ** 
@@ -23,20 +23,31 @@
 #ifndef LIBAEMB_H
 #define LIBAEMB_H
 
-#define AEMB_TXE 0x10000000
-#define AEMB_TX0 0x30000000
-#define AEMB_TX1 0x70000000
+#define AEMB_TXE 0x0100
+#define AEMB_TXP 0x0400
+#define AEMB_TX0 0x0300
+#define AEMB_TX1 0x0700
+#define AEMB_MSK 0x0F00
 
-#define AEMB_BIP 0x00000008
-#define AEMB_CCC 0x00000004
-#define AEMB_ITE 0x00000002
-#define AEMB_BLE 0x00000001
+#define AEMB_BIP 0x0008
+#define AEMB_CCC 0x0004
+#define AEMB_ITE 0x0002
+#define AEMB_BLE 0x0001
 
-void aemb_reboot () asm ("_program_init");
+
+// Linker symbols
+extern void* _STACK_SIZE;
+extern void* _stack_end;
+
+
+void aemb_hook_init () asm ("_program_init");
+void aemb_hook_clean () asm ("_program_clean");
+
 inline void aemb_enable_interrupt ();
 inline void aemb_disable_interrupt ();
 inline int aemb_isthread1();
 inline int aemb_isthread0();
+inline int aemb_isthreaded();
 
 /*!
 * Assembly macro to enable MSR_IE
@@ -68,96 +79,132 @@ void aemb_disable_interrupt ()
 
 /*!
 * Bootstrap Hook Override
-
-* It re-sizes the stack, allocates half to each thread and reboots.
 */
 
-void aemb_reboot ()
+void aemb_hook_init ()
 {
-  return;
-}
+  int msr, tmp;
+  int stk_end, stk_siz;
 
   /*
-  asm volatile (// Checks for TXE & BIP flags
-		"mfs     r4, rmsr;"
-		"andi    r3, r4, 0x10000008;"
-		"andi    r6, r4, 0x40000000;"
-		"xori    r18, r3, 0x10000000;"
-		"beqi    r18, 20;"
-		"andi    r4, r4, -9;"
-		"mts     rmsr, r4;"
-
-		// Returns when TXE=0 || BIP=1
-		"rtsd    r15, 8;"
-		"nop;"
-
-		// Calculate new stack
-		"addik   r3, r0, _STACK_SIZE;"  
-		"addik   r5, r0, _stack;"
-		"beqid   r6, 12;"        
-		"sra     r3, r3;"
-		"rsubk   r5, r3, r5;"
-		"addik   r5, r5, -16;"
-
-		// Re-allocate stack
-		"or      r1, r0, r5;"
-		"ori     r4, r4, 8;"
-		"mts     rmsr, r4;"
-
-		// Reboot
-		"brlid   r15, _crtinit;" 
-		"nop;"
-		"brai    exit;" 
-		);  
+  // Check to see if hardware threads are enabled
+  if (((msr & AEMB_TXE) != AEMB_TXE) ||
+  // Check to see if second thread is started
+      ((msr & AEMB_TX1) == AEMB_TX1)) return;
   */
+  //return;
+  asm volatile ("mfs %0, rmsr;"
+		// Check for BIP
+		"andi %1, %0, %5;"
+		"xori %1, %1, %5;"
+		"beqi %1, 44;"
 
-/*
-void aemb_reboot () 
-{ 
-  int stk_end, stk_siz;
-  int msr, tmp;
-  
-  asm volatile ("mfs %0, rmsr;":"=r"(msr));
- 
-  if ((msr & AEMB_BIP) || !(msr & AEMB_TXE)) 
-    {
-      msr &= ~AEMB_BIP;
-      asm volatile ("mts rmsr, %0;"::"r"(msr));
-    }
-  else
-    {
-      asm ("la %0, r0, _stack;" : "=r"(stk_end));
-      asm ("la %0, r0, _STACK_SIZE;" : "=r"(stk_siz));
+		// Check for TXE
+		"andi %1, %0, %2;"
+		"xori %1, %1, %2;"
+		"bnei %1, 36;"	
 
-      if (msr & AEMB_TX1) stk_end -= (stk_siz >> 1);	  
+		// Check for TX1
+		"andi %1, %0, %3;"
+		"xori %1, %1, %3;"
+		"beqi %1, 20;"
+		
+		// reallocate stack pointer for T0
+		"ori %1, r0, %4;"
+		"sra %1, %1;"
+		"rsubk r1, %1, r1;"
 
-      stk_end -= 16;	  
+		// reboot the machine
+		//"brki r0, _crtinit;"
+		"brid 0;"
+		"nop;"
 
-      asm ("or r1, r0, %0;" :: "r"(stk_end));
+		// clear BIP
+		"andni %1, %0, %5;"
+		"mts rmsr, %1;"
+		
+		:"=r"(msr), "=r"(tmp)
+		:"i"(AEMB_TXE), "i"(AEMB_TX1), "i"(&_STACK_SIZE), "i"(AEMB_BIP)
+		);
 
-      msr |= AEMB_BIP;
-      asm volatile ("mts rmsr, %0;"::"r"(msr));
-
-      asm ("brlid r15, _crtinit;"
-	   "nop;"
-	   "brai exit;"
-	   );
-    }    
 }
+
+/*!
+ Undo the changes made by programme init
 */
+
+void aemb_hook_clean ()
+{
+  int msr, tmp;
+  int stk_end, stk_siz;
+
+  /*
+  // Check to see if hardware threads are enabled
+  if (((msr & AEMB_TXE) != AEMB_TXE) ||
+  // Check to see if second thread is started
+      ((msr & AEMB_TX1) == AEMB_TX1)) return;
+  */
+  return;
+  asm volatile ("mfs %0, rmsr;"
+		"andi %1, %0, %2;"
+		"xori %1, %1, %2;"
+		"bnei %1, 28;"		
+		"andi %1, %0, %3;"
+		"xori %1, %1, %3;"
+		"beqi %1, 16;"
+		
+		// reallocate stack pointer for T0
+		"ori %1, r0, %4;"
+		"sra %1, %1;"
+		"addk r1, %1, r1;"
+
+		:"=r"(msr), "=r"(tmp)
+		:"i"(AEMB_TXE), "i"(AEMB_TX1), "i"(&_STACK_SIZE)
+		);
+
+  
+}
+
+/* Checks if it's T1, and returns 0 if true */
 
 int aemb_isthread1 ()
 {
-  int msr;
-  asm volatile ("mfs %0, rmsr;":"=r"(msr));
-  return (msr & AEMB_TX1);  
+  int msr, tmp, res;
+  asm volatile ("mfs %0, rmsr;"
+		"andi %1, %0, %3;"
+		"xori %1, %1, %3;"	       
+		:"=r"(msr), "=r"(tmp)
+		:"i"(AEMB_TXE), "i"(AEMB_TXP)
+		);
+  return tmp;  
 }
+
+/* Checks if it's T0, and returns 0 if true */
 
 int aemb_isthread0 ()
 {
-  int msr;
-  asm volatile ("mfs %0, rmsr;":"=r"(msr));
-  return (msr & AEMB_TX0);  
+  int msr, tmp, res;
+  asm volatile ("mfs %0, rmsr;"
+		"andi %1, %0, %3;"
+		"xori %1, %1, %2;"	       
+		:"=r"(msr), "=r"(tmp)
+		:"i"(AEMB_TXP), "i"(AEMB_MSK)
+		);
+  return tmp;  
+}
+
+/* Checks if TXE is available, and returns 0 if true */
+
+int aemb_isthreaded ()
+{
+  int msr, tmp, res;
+  asm volatile ("mfs %0, rmsr;"
+		"andi %1, %0, %2;"
+		"xori %1, %1, %2;"	       
+		:"=r"(msr), "=r"(tmp)
+		:"i"(AEMB_TXE), "i"(AEMB_MSK)
+		);
+  return tmp;  
 }
 
 #endif
