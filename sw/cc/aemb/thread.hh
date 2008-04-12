7b812bc59c880abc8f561562b751e685846ccace
sybreon <sybreon@55ffc1f0-3e4a-42ed-888c-7774f35025f9>
2008-04-12 14:07:26 +0000

    added a rendezvous function

--- sw/cc/aemb/thread.hh
+++ sw/cc/aemb/thread.hh
@@ -1,4 +1,4 @@
-/* $Id: thread.hh,v 1.3 2008-04-11 15:53:24 sybreon Exp $
+/* $Id: thread.hh,v 1.4 2008-04-12 14:07:26 sybreon Exp $
 ** 
 ** AEMB2 HI-PERFORMANCE CPU 
 ** Copyright (C) 2004-2007 Shawn Tan Ser Ngiap <shawn.tan@aeste.net>
@@ -35,8 +35,8 @@
 namespace aemb {
 
   /**
-  Checks to see if currently executing Thread 1
-  @return true if is Thread 1
+     Checks to see if currently executing Thread 1
+     @return true if is Thread 1
   */
   
   inline bool isThread1() 
@@ -46,8 +46,8 @@ namespace aemb {
   }
   
   /**
-  Checks to see if currently executing Thread 0
-  @return true if is Thread 0
+     Checks to see if currently executing Thread 0
+     @return true if is Thread 0
   */
   
   inline bool isThread0()
@@ -57,8 +57,8 @@ namespace aemb {
   }
   
   /**
-  Checks to see if it is multi-threaded or not
-  @return true if thread capable
+     Checks to see if it is multi-threaded or not
+     @return true if thread capable
   */
   inline bool isThreaded()
   {
@@ -71,36 +71,58 @@ namespace aemb {
   
   
   /**
-  Semaphore class
-  
-  Based on: Little Book of Semaphores, The - Downey, Allen B.
-
-  Presently implemented as software solution but a hardware one may be
-  required as the threads are hardware. This can be implemented using
-  a specialised add/sub/load register.
+     Semaphore class
+     
+     Based on: Little Book of Semaphores, The - Downey, Allen B.
+     
+     Presently implemented as software solution but a hardware one may be
+     required as the threads are hardware. This can be implemented using
+     a specialised add/sub/load register.
   */
   
   class semaphore {
+  private:
     volatile int _sem; ///< Semaphore in Memory
   public:
     /**
-    Preload the semaphore
-    @param pval preload value
+       Preload the semaphore
+       @param pval preload value
     */
     semaphore(int pval) { _sem = pval; } 
     
     /**
-    Increment the semaphore
+       Increment the semaphore
     */
     inline void signal() { _sem++; }
     
     /**
-    Decrement the semaphore and block if < 0
+       Decrement the semaphore and block if < 0
     */
     inline void wait() { _sem--; while (_sem < 0); } // block while
 						     // semaphore is
 						     // negative
   };
+
+  semaphore __mutex_rendezvous0(0); ///< internal rendezvous mutex
+  semaphore __mutex_rendezvous1(1); ///< internal rendezvous mutex
+
+  /**
+     Implements a simple rendezvous mechanism
+   */
+
+  void rendezvous()
+  {
+    if (isThread1())
+      {
+	__mutex_rendezvous0.wait();
+	__mutex_rendezvous1.signal();
+      }
+    else
+      {
+	__mutex_rendezvous0.signal();
+	__mutex_rendezvous1.wait();
+      }
+  }
   
 }
 
@@ -108,6 +130,9 @@ namespace aemb {
 
 /*
   $Log: not supported by cvs2svn $
+  Revision 1.3  2008/04/11 15:53:24  sybreon
+  changed MSR bits
+
   Revision 1.2  2008/04/11 11:34:30  sybreon
   changed semaphore case
 
